58ec23c0dcaff256d5cd9fee6d6f9b2e5655e35c
razaali572 <alirazaali5723@gmail.com>
2024-08-24 11:32:44 +0500

    d

--- connection/configs.php
+++
@@ -1,9 +0,0 @@
-<?php // configuration variables file
-
-// configuration variables for database connection
-const SERVER = 'localhost';
-const USER = 'root';
-const PASSWORD = '';
-const DB = '_wire_project_';
-
-?>
--- connection/connection.php
+++
@@ -1,6 +0,0 @@
-<?php // database connection file
-
-// database connection
-$conn = mysqli_connect(SERVER, USER, PASSWORD, DB);
-
-?>
