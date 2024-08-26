af75558f6d184364d2174d3db09140ef910bf52e
razaali572 <alirazaali5723@gmail.com>
2024-08-26 14:47:03 +0500

    modify

---
+++ viewproduct.php
@@ -0,0 +1,152 @@
+
+<?php // requiring the header of page
+require_once("includes/header.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/fevicon/fevicon.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>HandTime</title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!-- bs -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body> <!-- Moved the opening <body> tag here -->
+
+<!-- Section for supplier/wholesaler to list new items -->
+<section>
+
+    <div class="padding-ws-index">
+        <div class="row">
+            <div class="col-auto">
+                <div id="h2-ws-index">
+                    <h2 class="anton-regular">View Product</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card profile-header text-center">
+                    <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg" alt="John Smith">
+                    <h3>John Smith</h3>
+                    <p>Full Stack Developer</p>
+                    <p>Bay Area, San Francisco, CA</p>
+                    <button class="btn btn-primary btn-follow">Follow</button>
+                    <button class="btn btn-outline-light btn-message">Message</button>
+                </div>
+                <div class="card mt-3 social-links">
+                    <a href="#" class="text-center"><i class="fas fa-globe"></i> https://mdbootstrap.com</a>
+                    <a href="#" class="text-center"><i class="fab fa-github"></i> mdbootstrap</a>
+                    <a href="#" class="text-center"><i class="fab fa-twitter"></i> @mdbootstrap</a>
+                    <a href="#" class="text-center"><i class="fab fa-instagram"></i> mdbootstrap</a>
+                    <a href="#" class="text-center"><i class="fab fa-facebook"></i> mdbootstrap</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Full Name: Johnatan Smith</h5>
+                        <h5>Email: example@example.com</h5>
+                        <h5>Phone: (097) 234-5678</h5>
+                        <h5>Mobile: (098) 765-4321</h5>
+                        <h5>Address: Bay Area, San Francisco, CA</h5>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <div class="project-status">
+                            <h6>Assignment: Project Status</h6>
+                            <p>Web Design</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 80%;">80%</div>
+                            </div>
+                            <p>Website Markup</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 72%;">72%</div>
+                            </div>
+                            <p>One Page</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 89%;">89%</div>
+                            </div>
+                            <p>Mobile Template</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 55%;">55%</div>
+                            </div>
+                            <p>Backend API</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 66%;">66%</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <div class="project-status">
+                            <h6>Assignment: Project Status</h6>
+                            <p>Web Design</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 80%;">80%</div>
+                            </div>
+                            <p>Website Markup</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 72%;">72%</div>
+                            </div>
+                            <p>One Page</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 89%;">89%</div>
+                            </div>
+                            <p>Mobile Template</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 55%;">55%</div>
+                            </div>
+                            <p>Backend API</p>
+                            <div class="progress mb-2">
+                                <div class="progress-bar" style="width: 66%;">66%</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php // requiring the footer of page
+require_once("includes/footer.php");
+?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
